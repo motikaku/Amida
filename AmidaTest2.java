2114bdcadf7c961b62ec86f541e5bfd7ef659019
Yosuke Tsuji <yosuke.tsuji55@gmail.com>
2020-12-16 19:36:46 +0900

    簡単な説明とトレースを計算するメソッドを追加

--- AmidaTest2.java
+++ AmidaTest2.java
@@ -5,18 +5,22 @@ public class AmidaTest2{
 	public static void main(String args[]){
 		int tate = 5;
 		int maxSwaps = 5*(5-1)/2;
-		int maxYoko = maxSwaps;
+		int maxYoko = maxSwaps * maxSwaps * maxSwaps;
 		SecureRandom ran = new SecureRandom(); 
 		int d[] = new int[maxSwaps+1];
+		int t[] = new int[tate+1];
 		for(int i=0; i<maxSwaps+1; i++){
 			d[i]=0;
 		}
+		for(int i=0; i<tate+1; i++){
+			t[i]=0;
+		}
 		
 		
-		for(int i=0; i<6; i++){
-			int yoko = ran.nextInt(maxYoko);
+		for(int i=0; i<100000; i++){
+			int yoko = ran.nextInt(maxYoko+1);
 			aa1 Amida = new aa1(tate, yoko);
-			int[][] M = Amida.bodMat();
+			/*int[][] M = Amida.bodMat();
 			for(int j=0; j<tate; j++){
 				for(int k=0; k<tate; k++){
 					if (M[j][k] == -1)
@@ -26,14 +30,21 @@ public class AmidaTest2{
 				}
 				System.out.println();
 			}
-			System.out.println();
+			System.out.println();*/
 			int x = Amida.bod();
 			for(int j=0; j<maxSwaps+1; j++){
 				if(j == x) d[j]++;
 			}
+			
+			int y = Amida.calctrace();
+			t[y]++;
 		}
 		for(int i=0; i<maxSwaps+1; i++){
 			System.out.println("d["+i+"] = " + d[i]);
+		}
+		System.out.println();
+		for(int i=0; i<tate+1; i++){
+			System.out.println("t["+i+"] = " + t[i]);
 		}
 		
 	}
--- aa1.java
+++ aa1.java
@@ -7,7 +7,12 @@ class aa1{
 	static int h;
 	static int[] A;
 	
-	
+	/**
+	 * 1つのあみだくじを表すコンストラクタ
+	 * {@code 0}から{@code v-2}の間で，横棒がランダムに{@code h}回生成される
+	 * @param v {@code int}型．縦棒の本数
+	 * @param h {@code int}型．横棒の本数
+	 */
 	public aa1(int _v, int _h){
 		v = _v;
 		h = _h;
@@ -17,16 +22,25 @@ class aa1{
 			A[i] = S.nextInt(v-1);
 		}
 	}
-	
+	/**
+	 * 縦棒の本数{@code v}のゲッター
+	 */
 	public static int getV(){ return v; }
+	/**
+	 * 横棒の本数{@code h}のゲッター
+	 */
 	public static int getH(){ return h; }
+	/**
+	 * 横棒の配置場所を表す{@code int}型配列{@code A}のゲッター
+	 */
 	public static int[] getA(){ return A; }
 	
 	/**
-	 * (n,n)の正方行列式a*bの演算用のメソッド
-	 * @param a 左から掛ける行列
-	 * @param b 右から掛ける行列
-	 * @param n 行列の次元
+	 * (n,n)の正方行列同士の積a*bの演算用のメソッド
+	 * @param a {@code int[][]}型．左から掛ける行列
+	 * @param b {@code int[][]}型．右から掛ける行列
+	 * @param n {@code int}型．行列の次元
+	 * @return {@code int[][]}型を返す
 	 */
 	public static int[][] multiplyMat(int[][] a,int[][] b, int n){
 		int calc[][] = new int[n][n];
@@ -39,13 +53,22 @@ class aa1{
 		}
 		return calc;
 	}
-	
+	/**
+	 * 行列{@code M}の隣り合う行を交換するメソッド．
+	 * {@code a}番目の行と{@code a+1}番目の行を交換する．
+	 * @param M {@code int[][]}型．正方行列を表す二次元配列
+	 * @param a {@code int}型．交換する行が0から数えて何番目かを表す
+	 */
 	private static void swaprow(int[][] M, int a){
 		int B[] = M[a];
 		M[a] = M[a+1];
 		M[a+1] = B;
 	}
 	
+	/**
+	 * あみだくじの行列表示を返すメソッド．
+	 * @return {@code int[][]}型を返す．{@code v}文字の置換を表す正方行列を表す
+	 */
 	public static int[][] getMat(){
 		int[][] mat = new int[v][v];
 		for(int i=0; i<v; i++){
@@ -59,7 +82,11 @@ class aa1{
 		return mat;
 	}
 	
-	
+	/**
+	 * あみだくじが最短で何度の互換で恒等写像に出来るか（あみだくじのlength）
+	 * を{@code int}型で返すメソッド
+	 * @return {@code int}型であみだくじのlengthを返す．
+	 */
 	public static int bod(){
 		int i, j;
 		int count = 0;
@@ -84,6 +111,28 @@ class aa1{
 	}
 	
 	
+	/**
+	 * あみだくじの行列表示のトレースを返すメソッド．
+	 * @return {@code int}型．あみだくじの行列表示のトレース
+	 */
+	public static int calctrace(){
+		int i;
+		int count = 0;
+		int[][] M = getMat();
+		
+		for(i=0; i<v; i++){
+			count += M[i][i];
+		}
+		return count;
+	}
+	
+	
+	/**
+	 * あみだくじの行列表示からあみだくじのlengthが数えやすい行列を作るメソッド
+	 * 各行の{@code 1}の要素の右側の要素と下側の要素がすべて{@code 0}，
+	 * それ以外の要素は{@code -1}になっている．
+	 * @return {@code int[][]}型．正方行列
+	 */
 	public static int[][] bodMat(){
 		int i, j;
 		int count = 0;
